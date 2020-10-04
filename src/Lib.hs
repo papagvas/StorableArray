f9218bcdc03c8d7f47d0ba968e98dbf89a5abfc5
Gvasalia M <papagvasexy@gmail.com>
2020-10-05 00:23:45 +0300

    Tests needed, documentation added, job almost done

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,7 +11,7 @@ import qualified Data.ByteString.Base64 as Base64 (decode)
 import qualified Data.ByteString.Base16 as Base16 (decode)
 import qualified Data.ByteString.Internal as BSInt (toForeignPtr)
 import qualified Data.Char as Chr (chr)
-import qualified Data.Vector.Storable as SV (Vector, head, unsafeFromForeignPtr)
+import qualified Data.Vector.Storable as SV (Vector, head, unsafeFromForeignPtr, unsafeToForeignPtr)
 import qualified Language.Haskell.TH as TH (ExpQ, loc_filename, location) 
 import qualified Language.Haskell.TH.Quote as QQ (QuasiQuoter, QuasiQuoter (..), quoteExp, quoteDec, quotePat, quoteType)
 import qualified Text.ParserCombinators.Parsec as Parsec (Parser, oneOf, parse, many, spaces, many1, eof, char, string, choice, (<|>))
@@ -21,9 +21,12 @@ import qualified Data.Text as Text (pack)
 import qualified Text.URI as URI (renderBs, mkURI)
 import qualified Text.URI.QQ as URIQQ (uri)
 
-
 sizeOfElem :: (St.Storable a) => SV.Vector a -> Int
 sizeOfElem vec = St.sizeOf (undefined `asTypeOf` SV.head vec)
+
+-- | Convert @'ByteString'@ to @'Vector'@  
+-- alignment might be an issue, shamelessly copied from https://stackoverflow.com/questions/18682527/how-to-convert-between-bytestring-and-storable-vector
+-- i won't test bytestringtovector part since it was tested by the author of this code, proof: https://github.com/kmcallister/spool
 
 byteStringToVector :: (St.Storable a) => BStr.ByteString -> SV.Vector a
 byteStringToVector bs = vec where
@@ -31,23 +34,36 @@ byteStringToVector bs = vec where
     (fptr, off, len) = BSInt.toForeignPtr bs
     scale = (`div` sizeOfElem vec)
 
-mkSlice :: St.Storable a => Int -> Int -> SV.Vector a -> SV.Vector a
-mkSlice = undefined
+-- | Generate slice of the vector without copying it, O(1) complexity
+-- Storable Vector's slice is probably better to use....
 
-base64Alph = map Chr.chr $ [65..90] ++ [97..122] ++ [48..57] ++ [43,47]
+mkSlice :: St.Storable a => Int -> Int -> SV.Vector a -> SV.Vector a
+mkSlice start end vec = if (end - start + 1) > len then error "Len is lower than the slice length" else  SV.unsafeFromForeignPtr fptr (off + start * (sizeOfElem vec)) (end - start + 1)
+  where 
+    (fptr, off, len) =  SV.unsafeToForeignPtr vec  
+
+base64Alph = map Chr.chr $ [65..90] ++ [97..122] ++ [48..57] ++ [43,47] -- helper function, name is self-explanatory
+
+-- | Parses one base64 char excluding padding symbols
 
 checkBase64Chr :: Parsec.Parser Char
 checkBase64Chr = Parsec.oneOf base64Alph
 
---base64Table = [fst symWithCodes | symWithCodes <- zip [0 .. length base64Alph] base64Alph]
+-- | Parses base64 chars excluding padding symbols
 
 checkBase64Str :: Parsec.Parser String
-checkBase64Str = liftM concat $ Parsec.many1 $ Parsec.spaces >> Parsec.many1 checkBase64Chr
+checkBase64Str =  liftM concat $ Parsec.many1 $ Parsec.spaces >> Parsec.many1 checkBase64Chr
+
+-- | Decodes base64 str
+-- uses decode function from @'Data.ByteString.Base64'@
 
 base64Decode :: St.Storable a => String -> SV.Vector a
 base64Decode str = case Base64.decode $ Conv.toByteString' str of
   Left _ -> error "Please enter base64 encoded value"
   Right base64str -> byteStringToVector base64str
+
+-- | Decodes base64 str to @'ExpQ'@
+-- uses decode function from @'Data.ByteString.Base64'@
 
 base64Exp :: String -> TH.ExpQ 
 base64Exp str = do
@@ -58,6 +74,9 @@ base64Exp str = do
   where
     padding = (++ (if numOfPadding  < 3 then replicate numOfPadding '=' else ""))
     numOfPadding = length (filter (== '=') str)
+
+-- | base64 QuasiQuoter
+
 base64 :: QQ.QuasiQuoter 
 base64 = QQ.QuasiQuoter {
   QQ.quoteExp = base64Exp ,
@@ -66,13 +85,20 @@ base64 = QQ.QuasiQuoter {
   QQ.quoteType = undefined 
 }
 
-base16Alph = map Chr.chr $ [48..57] ++ [65..70]
+base16Alph = map Chr.chr $ [48..57] ++ [65..70] -- helper function
  
+-- | Parses one base16 char excluding padding symbols
+
 checkBase16Chr :: Parsec.Parser Char
 checkBase16Chr = Parsec.oneOf base16Alph
 
+-- | Parses base16 string excluding padding symbols
+
 checkBase16Str :: Parsec.Parser String
 checkBase16Str = liftM concat $ Parsec.many1 $ Parsec.spaces >> Parsec.many1 checkBase16Chr
+
+-- | Decodes base16 str
+-- uses decode function from @'Data.ByteString.Base16'@, which for some reason return tuple instead of @'Either String ByteString'@
 
 base16Decode :: St.Storable a => String -> SV.Vector a
 base16Decode str = if (snd base16Str == BStr.empty) then byteStringToVector $ fst base16Str else error "Please enter base16 encoded data"
@@ -81,12 +107,17 @@ base16Decode str = if (snd base16Str == BStr.empty) then byteStringToVector $ fs
  -- (BS.empty, base16Str) -> byteStringToVector base16Str
  -- (err, _) -> error $ Conv.fromByteString err
 
+-- | Decodes base16 str to @'ExpQ'@
+-- uses decode function from @'Data.ByteString.Base16'@, which for some reason return tuple instead of @'Either String ByteString'@
+
 base16Exp :: String -> TH.ExpQ 
 base16Exp str = do
   filename <- TH.loc_filename `fmap` TH.location
   case Parsec.parse checkBase16Str filename str of 
     Left _ -> error "Please enter base16 encoded value"
     Right properStr -> [|base16Decode properStr|]
+
+-- | base16 QuasiQuoter
 
 base16 :: QQ.QuasiQuoter 
 base16 = QQ.QuasiQuoter {
@@ -96,6 +127,7 @@ base16 = QQ.QuasiQuoter {
   QQ.quoteType = undefined 
 }
 
+-- | shamelessly using Text.Data.URI, no documentation needed, library was tested via quickcheck by the author, no tests needed for this part
 
 uriExp :: String -> TH.ExpQ
 uriExp str = case URI.mkURI (Text.pack str) of
