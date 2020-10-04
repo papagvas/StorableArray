b432ee8709c6c5ce35fb6c982968d2018bfe86d9
Gvasalia M <papagvasexy@gmail.com>
2020-10-04 18:12:55 +0300

    Almost done, refactoring needed

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,17 +5,21 @@
 module Lib where
 
 import           Control.Monad
-import qualified Data.ByteString as BStr 
+import qualified Data.ByteString as BStr (ByteString) 
 import qualified Data.ByteString.Conversion as Conv (toByteString')
 import qualified Data.ByteString.Base64 as Base64 (decode)
 import qualified Data.ByteString.Internal as BSInt (toForeignPtr)
 import qualified Data.Char as Chr (chr)
-import qualified Data.Vector.Storable as SV
-import qualified Language.Haskell.TH as TH 
-import qualified Language.Haskell.TH.Quote as QQ
+import qualified Data.Vector.Storable as SV (Vector, head, unsafeFromForeignPtr)
+import qualified Language.Haskell.TH as TH (ExpQ, loc_filename, location) 
+import qualified Language.Haskell.TH.Quote as QQ (QuasiQuoter, QuasiQuoter (..), quoteExp, quoteDec, quotePat, quoteType)
 import qualified Text.ParserCombinators.Parsec as Parsec (Parser, oneOf, parse, many, spaces, many1, eof, char, string, choice, (<|>))
-import qualified Foreign.Storable as St 
+import qualified Foreign.Storable as St (Storable, sizeOf) 
 import qualified Foreign.ForeignPtr as FPtr (castForeignPtr)
+import qualified Data.Text as Text (pack)
+import qualified Text.URI as URI (renderBs, mkURI)
+import qualified Text.URI.QQ as URIQQ (uri)
+
 
 sizeOfElem :: (St.Storable a) => SV.Vector a -> Int
 sizeOfElem vec = St.sizeOf (undefined `asTypeOf` SV.head vec)
@@ -39,17 +43,17 @@ checkBase64Chr = Parsec.oneOf base64Alph
 checkBase64Str :: Parsec.Parser String
 checkBase64Str = liftM concat $ Parsec.many1 $ Parsec.spaces >> Parsec.many1 checkBase64Chr
 
-whatever :: St.Storable a => String -> SV.Vector a
-whatever str = case Base64.decode $ Conv.toByteString' str of
-  Left _ -> error $ "whatever"
+base64Decode :: St.Storable a => String -> SV.Vector a
+base64Decode str = case Base64.decode $ Conv.toByteString' str of
+  Left _ -> error "Please enter base64 encoded value"
   Right base64str -> byteStringToVector base64str
 
 base64Exp :: String -> TH.ExpQ 
 base64Exp str = do
   filename <- TH.loc_filename `fmap` TH.location
   case liftM (++ (if length (filter (== '=') str) < 3 then replicate (length (filter (== '=') str)) '=' else "")) (Parsec.parse checkBase64Str filename str) of 
-    Left _ -> error $ "wat"
-    Right properStr -> [| whatever properStr |]
+    Left _ -> error "Please enter base64 encoded value"
+    Right properStr -> [|base64Decode properStr|]
 
 base64 :: QQ.QuasiQuoter 
 base64 = QQ.QuasiQuoter {
@@ -59,3 +63,15 @@ base64 = QQ.QuasiQuoter {
   QQ.quoteType = undefined 
 }
 
+uriExp :: String -> TH.ExpQ
+uriExp str = case URI.mkURI (Text.pack str) of
+  Nothing -> error "Please enter well-typed URI"
+  Just uri -> [| byteStringToVector $ URI.renderBs uri |]
+
+uri :: QQ.QuasiQuoter 
+uri = QQ.QuasiQuoter {
+  QQ.quoteExp = uriExp ,
+  QQ.quoteDec = undefined ,
+  QQ.quotePat = undefined ,
+  QQ.quoteType = undefined
+} 
